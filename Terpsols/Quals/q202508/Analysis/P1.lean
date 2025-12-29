b1f20ec33c63b231cb9715e0bb34386e78281701
Patrick Brosnan <pbrosnan@gmail.com>
2025-12-29 00:41:17 -0500

    Stated the first theorem for the first analysis problem, but with a sorry.

--- Terpsols/Quals/q202508/Analysis/P1.lean
+++ Terpsols/Quals/q202508/Analysis/P1.lean
@@ -1,5 +1,6 @@
 import Mathlib.MeasureTheory.MeasurableSpace.Defs
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+import Mathlib.Data.ENNReal.Basic
 
 
 noncomputable section Analysis_Problem_1
@@ -13,4 +14,28 @@ such that m(K) = a.
 
 open Set MeasureTheory
 
-variable {α : Type*} [MeasurableSpace α]
+--- variable {α : Type*} [MeasurableSpace α]
+
+variable {A : Set ℝ} {hA : MeasurableSet A}
+variable (gt : 0 < volume A)
+variable (lt : volume A < ⊤)
+variable (a : NNReal)
+
+noncomputable section
+
+open Set Filter MeasureTheory MeasureTheory.Measure TopologicalSpace
+
+#check (volume : Measure ℝ)
+
+#check (Ico a b)
+#check volume (Ico a b)
+#check volume A
+#check NNReal
+#check ENNReal
+
+example : (0 : ENNReal) < ⊤ := by norm_num
+example : ¬((⊤ : ENNReal) < ⊤) := by norm_num
+example (a : NNReal) : a < (⊤ : ENNReal) := ENNReal.coe_lt_top
+
+theorem exist_compact_meas_lt (lta : a < volume A) : ∃ K : Set ℝ,
+  K ⊆ A ∧ IsCompact K ∧ (a < volume K) := by sorry
