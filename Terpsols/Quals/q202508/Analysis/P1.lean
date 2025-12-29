aa339037653283ae664d31d5a0fddd720c17ffef
Patrick Brosnan <pbrosnan@gmail.com>
2025-12-28 23:26:03 -0500

    Adding P1.lean with a statement of Problem 1 from the Analysis Exam.

---
+++ Terpsols/Quals/q202508/Analysis/P1.lean
@@ -0,0 +1,5 @@
+/- 
+Problem 1. Let A be a Lebesgue measurable subset of R with 0 < m(A) < ∞, where m denotes the Lebesgue
+measure on R. Prove that for any positive real number a < m(A), there exists a compact set
+K ⊂ A such that m(K) = a.
+-/
