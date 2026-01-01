d2e7573592d724b48ab6e38c308cc39ca2c8e065
Patrick Brosnan <pbrosnan@umd.edu>
2025-12-31 20:53:26 -0500

    Added some lemmas on flight to Paris. But hard to prove without internet.

--- Terpsols/Quals/q202508/Analysis/P1.lean
+++ Terpsols/Quals/q202508/Analysis/P1.lean
@@ -2,6 +2,7 @@ import Mathlib.MeasureTheory.MeasurableSpace.Defs
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
 import Mathlib.MeasureTheory.Measure.Regular
 import Mathlib.Data.ENNReal.Basic
+import Mathlib.Topology.Compactness.Compact.lean
 
 noncomputable section Analysis_Problem_1
 
@@ -32,13 +33,13 @@ variable (a : NNReal)
 #check NNReal
 #check ENNReal
 #check volume.InnerRegular
+#check Icc 
 
 
 example : volume A = (volume : Measure ℝ) A := rfl
 example : (0 : ENNReal) < ⊤ := by norm_num
 example : ¬((⊤ : ENNReal) < ⊤) := by norm_num
 example (a : NNReal) : a < (⊤ : ENNReal) := ENNReal.coe_lt_top
-
 
 lemma exist_compact_meas_lt (lta : a < volume A) (hyp : MeasurableSet A) : ∃ K : Set ℝ,
   K ⊆ A ∧ IsCompact K ∧ (a < volume K) := by
@@ -47,3 +48,24 @@ lemma exist_compact_meas_lt (lta : a < volume A) (hyp : MeasurableSet A) : ∃ K
   apply MeasurableSet.exists_lt_isCompact
   ·  exact hyp
   ·  exact lta
+
+lemma compact_in_ball (K : Set ℝ) (com : IsCompact K) : ∃ r : NNReal,
+K ⊆ Icc (-r) r := by sorry 
+
+lemma intcompact (J K : Set ℝ) (cJ : IsCompact J) (cK : IsCompact K) :
+IsCompact J ∩ K := by
+  have : IsClosed J := IsCompact.isClosed cJ
+  exact?
+
+
+lemma compactmeas (K : Set ℝ) (cK : IsCompact K) : MeasurableSet K := by
+  exact IsCompact.measurableSet cK
+
+def measfun (K : Set ℝ) (r : NNReal) : ENNReal := volume (K ∩ (Icc (-r) r))
+
+lemma meascont (K : Set ℝ)  
+
+theorem exists_compact_eq (lta : a < volume A) (hyp : MeasurableSet A) : 
+∃ K : Set ℝ, K ⊆ A ∧ IsCompact K ∧ (a = volume K) := by 
+  obtain ⟨K, Ksub, Kcom, alt⟩ := exist_compact_meas_lt a lta hyp  
+  sorry
