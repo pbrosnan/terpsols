b78fbc7c9384b089346cac2e4acef628e2ac0fb5
Patrick Brosnan <pbrosnan@gmail.com>
2026-01-23 09:41:00 -0500

    Added Herstein problem.

--- Terpsols.lean
+++ Terpsols.lean
@@ -1,3 +1,5 @@
 import Terpsols.Basic
 import Terpsols.Quals.q202508.Algebra.A1
 import Terpsols.Quals.q202508.Analysis.P1
+import Terpsols.Math403_Algebra.Herstein_TopicsInAlgebra.Chapter1_PreliminaryNotions.p_01.03.01
+
