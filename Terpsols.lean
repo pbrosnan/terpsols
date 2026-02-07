f550383231a5edc4284e83b08801b609a88d7721
Patrick Brosnan <pbrosnan@gmail.com>
2026-02-07 15:56:07 -0500

    Adding Algebra P3 to Terpsols.lean.

--- Terpsols.lean
+++ Terpsols.lean
@@ -1,5 +1,5 @@
 import Terpsols.Basic
-import Terpsols.Quals.q202508.Algebra.A1
-import Terpsols.Quals.q202508.Analysis.P1
 import Terpsols.Math403_Algebra.Herstein_TopicsInAlgebra.Chapter1_PreliminaryNotions.p_03_01
-
+import Terpsols.Quals.q202508.Algebra.A1
+import Terpsols.Quals.q202508.Algebra.A3
+import Terpsols.Quals.q202508.Analysis.P1
