04ba8ae21229bd4eeba07597ccb6ed13a491796b
Patrick Brosnan <pbrosnan@gmail.com>
2026-02-06 21:34:07 -0500

    Finished first part of Algebra Problem 3 on Jacobson radical.

--- Terpsols/Quals/q202508/Algebra/A3.lean
+++ Terpsols/Quals/q202508/Algebra/A3.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 Patrick Brosnan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Patrick Brosnan
 -/
-import Mathlib
+import Mathlib.RingTheory.Jacobson.Ideal
 /-!
    # UMD Math Fall 2025 Algebra Qualifying Exam Problem 3
 
@@ -20,3 +20,45 @@ import Mathlib
    - `alg_p3_a` solves Part 1.
    - `alg_p3_b` solve Part 2.
 -/
+
+noncomputable section Algebra_P3
+open Ideal
+
+variable {R : Type*} [CommRing R]
+
+
+def J : Ideal R := sInf { I : Ideal R | IsMaximal I }
+
+lemma elts_of_J (x : R) : x ∈ J ↔ ∀ I : Ideal R, IsMaximal I → x ∈ I := by
+   constructor
+   ·  intro xin I Imax
+      simp only [J, Submodule.mem_sInf, Set.mem_setOf_eq] at xin
+      apply xin
+      exact Imax
+   ·  intro hyp
+      simp only [J, Submodule.mem_sInf, Set.mem_setOf_eq]
+      exact hyp
+
+lemma J_is_jacobson_bot : J = jacobson (⊥ : Ideal R) := by
+   simp only [J, jacobson]
+   simp
+
+theorem alg_p3_a {x : R} (h : x ∈ J) : IsUnit (1 + x) := by
+   rw [J_is_jacobson_bot] at h
+   have : (1 + x) - 1 ∈ jacobson (⊥ : Ideal R) := by
+      simp only [add_sub_cancel_left]; exact h
+   apply isUnit_of_sub_one_mem_jacobson_bot
+   exact this
+
+
+
+
+
+
+
+
+
+
+
+end Algebra_P3
+--- #min_imports
